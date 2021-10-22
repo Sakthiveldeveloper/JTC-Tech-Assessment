b4418cea0521292b4b5ccf5161a459bc30b53699
Vignesh Shenoy <92209607+Vignesh-shenoy-10@users.noreply.github.com>
2021-10-22 18:15:29 +0530

    Updated solution.js with comments
    Comments added

--- solution.js
+++ solution.js
@@ -2,14 +2,14 @@ function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
 
 
-  while (currentIndex !== 0) {
+  while (currentIndex !== 0) {   //Current index starts from 0th position of array
 
  
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex = Math.floor(Math.random() * currentIndex); //randomize the index of the array to get a random index within the input array
     currentIndex--;
 
 
-    [array[currentIndex], array[randomIndex]] = [
+    [array[currentIndex], array[randomIndex]] = [ //current is 0, random  = random,0
       array[randomIndex], array[currentIndex]];
   }
 
